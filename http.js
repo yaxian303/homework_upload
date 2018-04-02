8456862dcbf74d9feb24ac1c9692a2826c7c17dc
QingYaxian <yaxian303@qq.com>
2018-04-02 22:34:21 +0800

    first

---
+++ http.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+var express = require('express')
+  , path = require('path')
+  , app = express()
+  , logger = require('morgan')
+  , multer  = require('multer');
+
+
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'www')));
+app.set('views', path.join(__dirname, 'www'));
+
+
+/* GET home page. */
+app.get('/', function(req, res, next) {
+    res.render('index', { title: '上传作业' });
+});
+
+
+/* config the multer storage */
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './www/homeworkfiles')//设置存储地址
+    },
+    
+    filename: function (req, file, cb) {
+        var fileFormat = (file.originalname).split(".");
+        var myDate = new Date();//加上时间戳的文件名
+        var time=(myDate.getMonth()+1)+"月"+myDate.getDate()+"日"+myDate.getHours()+"时"+myDate.getMinutes()+"分"; //获取当前小时数(0-23)
+        cb(null, fileFormat[0] +"-"+ time + "." + fileFormat[1]);
+    }
+});
+
+var upload = multer({storage: storage});
+
+//接受上传参数
+app.post('/upload', upload.single('file'), function(req, res) {  
+    if (req.file) {
+   
+        res.send("祁老师收到你的作业了。");
+        console.log(req.file);
+    }
+});
+
+app.listen(80, function() {
+    console.log('Server start on :' + '80');
+});
---
+++ www/js/upload.js
@@ -0,0 +1,108 @@
+﻿$(function () {
+   if (!!window.ActiveXObject || "ActiveXObject" in window){//IE 
+   alert("请使用谷歌浏览器！"); 
+  }
+   
+    var files = [];
+    var ctr = {    
+        /**
+         * upload
+         * @param {Object} file: file.name, file.dataURL
+        */
+        upload: function (file){
+          
+             //修改以下filename
+           
+            var fd = new FormData();
+            fd.append('file', file);
+            
+            $.ajax({
+                type: 'POST',
+                url: '/upload',
+                data: fd,
+                processData: false,
+                contentType: false,
+                xhr: function() {
+                    var xhr = new window.XMLHttpRequest();
+                    xhr.upload.addEventListener("progress", function(evt) {
+                        if (evt.lengthComputable) {
+                            var percentComplete = evt.loaded / evt.total * 100;
+                            //在 Chrome DevTool 中限制网速来模拟效果比较好
+                            $('.progress-bar').css('width',percentComplete + '%').html(percentComplete + '% Complete');
+                        }
+                    }, false);
+               
+                    return xhr;
+                }
+            }).success(function (res) {
+               //对话框弹出上传成功;
+               alert(res);
+            }).error(function (err) {
+                //对话框弹出上传失败;
+               alert("上传失败，请重新上传！");
+            });
+        },
+
+
+    };
+
+    $('#inputFile').on('change', function (e) {
+        var file = e.target.files[0];
+        if (file) {
+            //只许上传word文件或者压缩文件 
+            var fileName=file.name;    
+            var fileType = (fileName.substring(fileName.lastIndexOf(".")+1,fileName.length)).toLowerCase();
+            
+
+            if (fileType=="doc"||fileType=="docx"||fileType=="rar"||fileType=="zip") {
+                var reader = new FileReader();
+                //上传文件
+                reader.onerror = function () {
+                    console.error('reader error');
+                };
+                files.push(file);
+
+                // 读出文件路径
+                reader.readAsDataURL(file);
+
+            } 
+        }else {
+            throw '请选择有效的文件';
+        }
+    });
+
+    $('.btn-primary').on('click', function (e) {
+        if (files.length === 0) {
+            alert('请选择上传文件！');
+            return;
+        }   
+  
+      var num=  $('#stuNO').val();
+      var name= $('#stuName').val();
+      var worknum= $('#workNO').val();
+      if(worknum){
+            //判断输入是否符合规则
+        if(/^\d{11}$/i.test(num)&&/^[\u4e00-\u9fa5]{2,12}$/i.test(name)){
+        
+            var oriname=files[0].name.split(".");
+            //重新命名文件再上传
+            var filename=num+"-"+name+"-"+worknum+"."+oriname[1];
+            var newfile = new File([files[0]],filename);
+            ctr.upload(newfile);
+    
+        }
+        else{
+            alert("请查看学号是否输入正确");
+        }
+
+      }
+      else{
+        alert("请选择这是第几次作业");
+      }
+     
+       
+      
+    });
+
+    
+});
